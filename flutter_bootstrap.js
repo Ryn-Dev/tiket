a9d71be4c893b60a50126648bedf6a3c71500644
Febryan1453 <brayenfebryan@gmail.com>
2025-05-06 20:01:14 +0700

    feat: Change category to criteria

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1306964056"
+    serviceWorkerVersion: "2748659"
   }
 });
